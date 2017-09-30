dca92eb0b8be0a6724bbe103556aca99870983b2
minimallinux <anthonymcgowan1@gmail.com>
2017-09-30 10:25:25 +0100

    Update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+var webpackLoadPlugins = ({
+    pattern: ['*-webpack-plugin','@*/*-webpack-plugin'], // the glob(s) to search for
+    config: 'package.json', // where to find the plugins, by default searched up from process.cwd()
+    scope: ['dependencies', 'devDependencies', 'peerDependencies'], // which keys in the config to look within
+    replaceString: /-webpack-plugin$/, // what to remove from the name of the module when adding it to the context
+  });
 var messages = require('./messages');
-var app = document.querySelector('#app');
+var app = document.querySelector('#page');
 app.innerHTML = "<p>" + messages.hi + messages.event + '</p>';
