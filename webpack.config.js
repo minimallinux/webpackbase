da8dc7e11d5bd608e2fc31ae6d2cfa3aa4e5549a
minimallinux <anthonymcgowan1@gmail.com>
2017-09-30 12:01:37 +0100

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 var path = require('path');
 var plugins = require('webpack-load-plugins')();
 module.exports = {
-  entry: './src/',
-  output: {
-    path: path.join(__dirname, 'dist'),
-    publicPath: '/dist/',
-    filename: 'bundle.js'
-  }
+entry: './src/',
+ output: {
+ path: path.join(__dirname, 'dist'),
+ publicPath: '/dist/',
+ filename: 'bundle.js'
+ }
 }
